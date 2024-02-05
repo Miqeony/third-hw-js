69ef0d514d55e8c5aa415fcb7c5292417a7625fe
Miqeony <154737220+Miqeony@users.noreply.github.com>
2024-02-05 21:38:45 +0200

    Update 02-task.js

--- js/02-task.js
+++ js/02-task.js
@@ -3,7 +3,6 @@ const arrayB = [3, 4, 5, 6, 7];
 
 const togetherArray = arrayA.concat(arrayB);
 
-
 const uniqueArray = [];
 for (let i = 0; i < togetherArray.length; i++){
     if (uniqueArray.indexOf(togetherArray[i]) === -1) {
